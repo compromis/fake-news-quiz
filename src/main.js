c670bdcf038dbef900506b7680e1acc4e681b3e4
Guillem <guillem@disedit.com>
2017-12-27 10:18:50 +0100

    Add vue-smooth-scroll

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import vueSmoothScroll from 'vue-smooth-scroll'
 import Quiz from './Quiz'
 
 Vue.config.productionTip = false
+Vue.use(vueSmoothScroll)
 
 /* eslint-disable no-new */
 new Vue({
