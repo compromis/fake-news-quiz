2207bba24981e7ad76caa6590319aba33f58eff4
Guillem <guillem@disedit.com>
2017-12-28 11:17:56 +0100

    Enhance CSS responsiveness in the results component

--- src/components/QuizResults.vue
+++ src/components/QuizResults.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="results" id="results">
-    <Party class="party-hat" />
-    <h3>Has encertat {{ correctAnswers }} de {{ totalQuestions }}</h3>
-    <img :src="result.gif" alt="Gif reacció" />
-    <p>{{ result.message }}</p>
-    <hr />
-    <p class="share-cta">Comparteix la teua puntuació<br />i reta a les teues amistats</p>
-    <p class="social-plugins">
-      <iframe src="https://www.facebook.com/plugins/like.php?href=https://compromis.net/fake-news&width=198&layout=button_count&action=like&size=large&show_faces=false&share=true&height=37&appId=252515324782640&locale=ca_ES" width="213" allowtransparency="true" scrolling="no" class="social-plugins-facebook" frameborder="0" height="30"></iframe>
-      <iframe :src="'https://platform.twitter.com/widgets/tweet_button.html?size=l&url=https://compromis.net/fake-news&via=compromis&related=compromis&text=' + encodeURIComponent(tweet) + '&lang=es'" class="social-plugins-twitter" width="100" height="30" allowtransparency="true" scrolling="no" frameborder="0"></iframe>
-    </p>
+  <div class="results-wrapper">
+    <div class="results" id="results">
+      <Party class="party-hat" />
+      <h3>Has encertat {{ correctAnswers }} de {{ totalQuestions }}</h3>
+      <img :src="result.gif" alt="Gif reacció" />
+      <p>{{ result.message }}</p>
+      <hr />
+      <p class="share-cta">Comparteix la teua puntuació<br />i reta a les teues amistats</p>
+      <p class="social-plugins">
+        <iframe src="https://www.facebook.com/plugins/like.php?href=https://compromis.net/fake-news&width=198&layout=button_count&action=like&size=large&show_faces=false&share=true&height=37&appId=252515324782640&locale=ca_ES" width="213" allowtransparency="true" scrolling="no" class="social-plugins-facebook" frameborder="0" height="30"></iframe>
+        <iframe :src="'https://platform.twitter.com/widgets/tweet_button.html?size=l&url=https://compromis.net/fake-news&via=compromis&related=compromis&text=' + encodeURIComponent(tweet) + '&lang=es'" class="social-plugins-twitter" width="100" height="30" allowtransparency="true" scrolling="no" frameborder="0"></iframe>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -44,10 +46,14 @@ export default {
 <style lang="scss" scoped>
 @import '../variables';
 
-.results {
-  position: relative;
+.results-wrapper {
   max-width: 600px;
   margin: 0 auto;
+  padding: 1rem;
+}
+
+.results {
+  position: relative;
   background: $white;
   padding: 1.5rem;
   border-radius: 10px;
@@ -61,7 +67,7 @@ export default {
   }
 
   p {
-    font-size: 1.15rem;
+    font-size: 1rem;
     max-width: 400px;
     margin: 0 auto;
   }
@@ -86,5 +92,21 @@ export default {
   fill: $text-color-dark;
   width: 80px;
   height: 80px;
+  margin: 0 auto;
 }
+
+@media (min-width: 769px) {
+  .results {
+    p {
+      font-size: 1.15rem;
+    }
+  }
+}
+
+@media (max-height: 700px) {
+  .party-hat {
+    display: none;
+  }
+}
+
 </style>
