1edf2ea510c2da633d7540cf50a7c4d7ffd259bc
Guillem <guillem@disedit.com>
2017-12-28 11:10:56 +0100

    Add share buttons when result is shown

--- src/components/QuizResults.vue
+++ src/components/QuizResults.vue
@@ -4,6 +4,12 @@
     <h3>Has encertat {{ correctAnswers }} de {{ totalQuestions }}</h3>
     <img :src="result.gif" alt="Gif reacció" />
     <p>{{ result.message }}</p>
+    <hr />
+    <p class="share-cta">Comparteix la teua puntuació<br />i reta a les teues amistats</p>
+    <p class="social-plugins">
+      <iframe src="https://www.facebook.com/plugins/like.php?href=https://compromis.net/fake-news&width=198&layout=button_count&action=like&size=large&show_faces=false&share=true&height=37&appId=252515324782640&locale=ca_ES" width="213" allowtransparency="true" scrolling="no" class="social-plugins-facebook" frameborder="0" height="30"></iframe>
+      <iframe :src="'https://platform.twitter.com/widgets/tweet_button.html?size=l&url=https://compromis.net/fake-news&via=compromis&related=compromis&text=' + encodeURIComponent(tweet) + '&lang=es'" class="social-plugins-twitter" width="100" height="30" allowtransparency="true" scrolling="no" frameborder="0"></iframe>
+    </p>
   </div>
 </template>
 
@@ -26,6 +32,10 @@ export default {
   computed: {
     result: function () {
       return this.results.find((element) => this.correctAnswers >= element.minPoints && this.correctAnswers <= element.maxPoints)
+    },
+
+    tweet: function () {
+      return 'He tret un ' + this.correctAnswers + ' de ' + this.totalQuestions + ' en el Quiz de Fake News de @compromis. Pots superar-me?'
     }
   }
 }
@@ -60,6 +70,15 @@ export default {
     border-radius: 5px;
     margin: 1rem 0;
     max-width: 100%;
+  }
+
+  .share-cta {
+    font-weight: bold;
+    margin-top: 2rem;
+  }
+
+  .social-plugins {
+    margin-top: 1rem;
   }
 }
 
