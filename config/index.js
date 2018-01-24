0fc3f1e1e8bf085b2678e53ddc19e767149ecfeb
Guillem <guillem@disedit.com>
2018-01-24 16:08:02 +0100

    Update production assets path

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/fake-news/',
+    assetsPublicPath: '/quiz/fake-news/',
 
     /**
      * Source Maps
